37cc63b6f30454ac06cd3e4acff47c74e3ecc575
iskrenradev <iskrenr005@gmail.com>
2023-10-13 11:28:50 +0200

    Initial commit

---
+++ src/main/java/de/ir/springwebcoding/SpringWebCodingApplication.java
@@ -0,0 +1,13 @@
+package de.ir.springwebcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/ir/springwebcoding/SpringWebCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ir.springwebcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
